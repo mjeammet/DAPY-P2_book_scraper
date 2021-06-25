9ea4d8dbe19b81ee20d7e2702253856f6b245207
mjeammet <marie.jeammet@gmail.com>
2021-06-25 20:18:13 +0200

    :page_with_curl: handling multi-pages categories

--- price_extraction.py
+++ price_extraction.py
@@ -8,7 +8,7 @@ from datetime import datetime as dt
 from bs4 import BeautifulSoup
 import re # just used once, à voir si ça reste
 import math
-import pandas
+import pandas # pour l'export en differentes sheets
 
 # partly inspired from https://www.ionos.fr/digitalguide/sites-internet/developpement-web/web-scraping-avec-python/
 # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#modifying-string
@@ -27,7 +27,7 @@ def export_to_csv(book_info_table):
 
     # ! verifier que len(book_info_table)-1 est bien égal au nombre total de bouquins dans la catégorie
     exported_file_name = 'export_' + dt.now().strftime('%Y%m%d-%H%M%S') + '.csv'
-    print(len(book_info_table)-1, 'books added to "' + exported_file_name + '')
+    print(len(book_info_table)-1, 'books of category added to "' + exported_file_name + '')
     with open(exported_file_name, 'w', newline='') as csvfile:
         my_writer = csv.writer(csvfile, delimiter=delim)
         my_writer.writerows(book_info_table)
@@ -44,7 +44,7 @@ if response.ok:
     # Get the list of all categories and browse it
     category_list = html.find_all("ul")[2].find_all('a')
 
-    for category in category_list[0:2]: # sublist the list for testing purposes
+    for category in category_list[3:4]: # sublist the list for testing purposes
         category_url = index_url + category['href']
         category_page = BeautifulSoup(requests.get(category_url).text, 'html.parser')
 
@@ -68,13 +68,19 @@ if response.ok:
         #     # category_url.replace('index.html', 'page-2.html')
         #     nb_pages = category_page.find(class_='pager').li.text.rstrip()[-1]
 
-        for page in range(2,nb_pages_in_category+1):
-            page_url = index_url + category['href'].replace('index.html', 'page-' + str(page) + '.html')
-            category_page.append(BeautifulSoup(requests.get(page_url).text, 'html.parser'))
-
         # Getting all books on this page
-        category_page.find_all('article')
-        for article_html in category_page.find_all('article'):
+        category_articles_list = category_page.find_all('article')
+
+        # if category has more than 20 books, add the other pages' books to category_bookslist
+        for extra_page_nb in range(2,nb_pages_in_category+1):
+            extra_page_url = index_url + category['href'].replace('index.html', 'page-' + str(extra_page_nb) + '.html')
+            extra_books = BeautifulSoup(requests.get(extra_page_url).text, 'html.parser').find_all('article')
+            category_articles_list.extend(extra_books)
+            # print("category_bookslist has", len(category_articles_list), "books. ")
+
+        # Browse category_articles_list
+        # which contains all books of this category
+        for article_html in category_articles_list:
             product_page_url = article_html.a['href'].replace('../../../', index_url + 'catalogue/')
             product_page = BeautifulSoup(requests.get(product_page_url).text, 'html.parser')
             product_page_article = product_page.body.article
@@ -95,8 +101,7 @@ if response.ok:
 
             input_variable.append([product_page_url, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
 
-        #export_to_csv(input_variable)
-        print(len(input_variable)-1)
+        export_to_csv(input_variable)
 
 print("\n==============================\n\tTEST AREA\n==============================")
 print("->", '')
